e53c95a8633cdb023faecd67c330a76a43ac0786
shauniebyrne <byr21002@byui.edu>
2023-05-10 14:48:30 -0700

    video 2 done but not working

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,18 @@
-const passport = require('passport');
-
 const router = require('express').Router();
+const passport = require('passport');
 
 router.use('/', require('./swagger'));
 router.use('/user', require('./user'));
 router.use('/authors', require('./authors'));
 router.use('/read', require('./read'));
 
-router.get('/login', passport.authenticate('github', (req, res) => {}));
+router.get('/login', passport.authenticate('github'), (req, res) => {});
 
 router.get('/logout', function(req, res, next) {
-    req.logOut(function(err) {
-        if (err) {return next(err);}
-        res.redirect('/');
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      res.redirect('/');
     });
-});
+  });
 
 module.exports = router;
